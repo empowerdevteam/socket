eefc4c12642854b37156940dd9b14f94f2c0a46d
Arpita <arpita.jena@empower-solutions.co.in>
2020-08-25 14:05:01 +0530

    initial commit

---
+++ src/ios/Socket.m
@@ -0,0 +1,28 @@
+/********* Socket.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface Socket : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation Socket
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
